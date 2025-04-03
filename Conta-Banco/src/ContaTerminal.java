60a174500b50b32b326db79f9fe4c69af4e58e09
Richard <rrichard.rodrigues2@gmail.com>
2025-04-03 12:56:44 -0300

    Desafio Dio (Conta bancária)

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite o número da  agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o número da conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Digite o saldo da conta: ");
+        double saldo = scanner.nextDouble();
+
+
+        System.out.println("\n");
+
+        System.out.println("Olá "+nome+ ", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+ ", conta "+numeroConta+" e seu saldo "+saldo+" já está disponível para saque.");
+
     }
 }
